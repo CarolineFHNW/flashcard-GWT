cd58d8dc773aeee4e7dfc69560deb85acfd4a9c0
CarolineFHNW <caroline.schnell@students.fhnw.ch>
2013-11-26 11:06:24 +0100

    Original Lektion 2

---
+++ src/webfr/flashcards/client/FlashcardsGWT.java
@@ -0,0 +1,68 @@
+package webfr.flashcards.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class FlashcardsGWT implements EntryPoint {
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		// This method will contain code to show 
+	  // the application in the browser
+	  
+	  
+// TODO: uncomment the next block of code and read the worksheet!
+//    final LoginForm welcome = new LoginForm("Change this text");
+//
+//    // Add the nameField and sendButton to the RootPanel
+//    // Use RootPanel.get() to get the entire body element
+//    RootPanel.get("loginFormContainer").add(welcome);
+//
+//    // Create the popup dialog box
+//    final MyDialogBox dialogBox = new MyDialogBox();
+//
+//    // Create a handler for the sendButton and nameField
+//    class MyHandler implements ClickHandler {
+//      /**
+//       * Fired when the user clicks on the sendButton.
+//       */
+//      public void onClick(ClickEvent event) {
+//        sendNameToServer();
+//      }
+//
+//      /**
+//       * Send the name from the nameField to the server and wait for a response.
+//       */
+//      private void sendNameToServer() {
+//        String textToServer = welcome.getText();
+//
+//        // Then, we send the input to the server.
+//        dialogBox.setTextToServer(textToServer);
+//        dialogBox.setServerResponse("", false);
+//        greetingService.greetServer(textToServer, new AsyncCallback<String>() {
+//          public void onFailure(Throwable caught) {
+//            // Show the RPC error message to the user
+//            dialogBox.setText("Remote Procedure Call - Failure");
+//            dialogBox.setServerResponse(SERVER_ERROR, true);
+//            dialogBox.center();
+//          }
+//
+//          public void onSuccess(String result) {
+//            dialogBox.setText("Remote Procedure Call");
+//            dialogBox.setServerResponse(result, false);
+//            dialogBox.center();
+//          }
+//        });
+//      }
+//    }
+//
+//    // Add a handler to send the name to the server
+//    MyHandler handler = new MyHandler();
+//    // TODO: register handler with your login form
+	  
+	}
+}
